e83dd54c9dba218caffd778dd67b2ca78bc79b6a
Miekole <Miekole.ferrazzo@gmail.com>
2021-01-19 16:38:05 -0500

    test file
    added

---
+++ Test/Example/Example/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	system("Color 0C");
+
+	cout << "=============\n ";
+	cout << "Hello World\n";
+	cout << "=============\n ";
+	cout << "\n";
+	cout << "Press enter.";
+
+	cin.get();
+	return 0;
+}
